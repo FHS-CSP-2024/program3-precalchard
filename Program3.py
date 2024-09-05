f32fa91408a1f7e65186f88cd4ec86f2a9d79df6
precalchard <seaphantom07@gmail.com>
2024-09-05 23:30:01 +0000

    working on Program 3.py

--- Program3.py
+++ Program3.py
@@ -24,10 +24,11 @@ upper = 3000
 
 print("my name is ", name, " , I am ", age, "years old")
 print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+print("- " + skill1 + " (" + level1 + ")")
+print("- " + skill2 + " (",level2 + ")")
+print("- " + skill3 + " (" + level3 + ")" '\n\n')
+
+print(f"I am looking for a job with a salary of {lower} - {upper} dollars per month")
 
 
 
